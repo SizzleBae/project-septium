2412bc77c3d6fe0fbb26e6b62ec770d2133767db
SizzleBae <eragongreat@outlook.com>
2020-01-10 20:24:13 +0100

    Some work on interactable TileEntity

--- src/main/java/com/sizzlebae/projectseptium/block/QuartzAssemblerBlock.java
+++ src/main/java/com/sizzlebae/projectseptium/block/QuartzAssemblerBlock.java
@@ -2,13 +2,21 @@ package com.sizzlebae.projectseptium.block;
 
 import com.sizzlebae.projectseptium.init.ModTileEntityTypes;
 
+import com.sizzlebae.projectseptium.tileentity.QuartzAssemblerTileEntity;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalBlock;
+import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.state.StateContainer.Builder;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.World;
+import net.minecraftforge.items.ItemStackHandler;
 
 public class QuartzAssemblerBlock extends HorizontalBlock {
 
@@ -16,6 +24,37 @@ public class QuartzAssemblerBlock extends HorizontalBlock {
         super(properties);
 
         this.setDefaultState(this.getDefaultState().with(HORIZONTAL_FACING, Direction.NORTH));
+    }
+    /**
+     * Called on the logical server when a BlockState with a TileEntity is replaced by another BlockState.
+     * We use this method to drop all the items from our tile entity's inventory and update comparators near our block.
+     *
+     * Implementing/overriding is fine.
+     */
+    @Override
+    public void onReplaced(BlockState oldState, World worldIn, BlockPos pos, BlockState newState, boolean isMoving) {
+        if(oldState.getBlock() != newState.getBlock()) {
+            TileEntity tileEntity = worldIn.getTileEntity(pos);
+            if(tileEntity instanceof QuartzAssemblerTileEntity) {
+                final ItemStackHandler inventory = ((QuartzAssemblerTileEntity) tileEntity).inventory;
+
+                for(int slot = 0; slot < inventory.getSlots(); slot++) {
+                    InventoryHelper.spawnItemStack(worldIn, pos.getX(), pos.getY(), pos.getZ(), inventory.getStackInSlot(slot));
+                }
+            }
+        }
+
+        super.onReplaced(oldState, worldIn, pos, newState, isMoving);
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, Rotation rot) {
+        return state.with(HORIZONTAL_FACING, rot.rotate(state.get(HORIZONTAL_FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, Mirror mirrorIn) {
+        return state.rotate(mirrorIn.toRotation(state.get(HORIZONTAL_FACING)));
     }
 
     @Override
--- src/main/java/com/sizzlebae/projectseptium/tileentity/QuartzAssemblerTileEntity.java
+++ src/main/java/com/sizzlebae/projectseptium/tileentity/QuartzAssemblerTileEntity.java
@@ -3,24 +3,41 @@ package com.sizzlebae.projectseptium.tileentity;
 import java.util.Optional;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
+import com.sizzlebae.projectseptium.init.ModBlocks;
 import com.sizzlebae.projectseptium.init.ModTileEntityTypes;
 
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.inventory.InventoryHelper;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.AbstractCookingRecipe;
 import net.minecraft.item.crafting.FurnaceRecipe;
 import net.minecraft.item.crafting.IRecipeType;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Direction;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandlerModifiable;
 import net.minecraftforge.items.ItemStackHandler;
+import net.minecraftforge.items.wrapper.RangedWrapper;
 
-public class QuartzAssemblerTileEntity extends TileEntity implements ITickableTileEntity {
+public class QuartzAssemblerTileEntity extends TileEntity implements ITickableTileEntity, INamedContainerProvider {
 
     public static final int INPUT_SLOT = 0;
     public static final int OUTPUT_SLOT = 1;
+
+    private static final String INVENTORY_TAG = "inventory";
 
     public final ItemStackHandler inventory = new ItemStackHandler(2) {
         @Override
@@ -44,6 +61,10 @@ public class QuartzAssemblerTileEntity extends TileEntity implements ITickableTi
     };
 
     private final LazyOptional<ItemStackHandler> inventoryCapabilityExternal = LazyOptional.of(() -> this.inventory);
+
+    private final LazyOptional<IItemHandlerModifiable> inventoryCapabilityExternalUpAndSides = LazyOptional.of(() -> new RangedWrapper(this.inventory, INPUT_SLOT, INPUT_SLOT + 1));
+
+    private final LazyOptional<IItemHandlerModifiable> inventoryCapabilityExternalDown = LazyOptional.of(() -> new RangedWrapper(this.inventory, OUTPUT_SLOT, OUTPUT_SLOT + 1));
 
     public QuartzAssemblerTileEntity() {
         super(ModTileEntityTypes.QUARTZ_ASSEMBLER);
@@ -109,15 +130,50 @@ public class QuartzAssemblerTileEntity extends TileEntity implements ITickableTi
      * @param stack The stack to try to insert
      */
     private void insertOrDropStack(final int slot, final ItemStack stack) {
-        final boolean canInsertContainterItemIntoSlot = inventory.insertItem(slot, stack, true).isEmpty();
+        final boolean canInsertContainerItemIntoSlot = inventory.insertItem(slot, stack, true).isEmpty();
 
         // TODO: Fill up space instead of ignoring stacks that are not completely
         // inserted
-        if (canInsertContainterItemIntoSlot) {
+        if (canInsertContainerItemIntoSlot) {
             inventory.insertItem(slot, stack, false);
         } else {
             InventoryHelper.spawnItemStack(world, pos.getX(), pos.getY(), pos.getZ(), stack);
         }
+    }
+
+    /**
+     * Mimics the code inAbstractFurnaceTileEntity#func_214005_h()
+     *
+     * @return The custom smelt time or 200 if there is no recipe for the input
+     */
+    private short getSmeltTime(final ItemStack input) {
+        return getRecipe(input)
+                .map(AbstractCookingRecipe::getCookTime)
+                .orElse(200)
+                .shortValue();
+    }
+
+    @Nonnull
+    @Override
+    public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
+        if(cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+            if(side == null) {
+                return inventoryCapabilityExternal.cast();
+            }
+
+            switch(side) {
+                case DOWN:
+                    return inventoryCapabilityExternalDown.cast();
+                case UP:
+                case NORTH:
+                case SOUTH:
+                case EAST:
+                case WEST:
+                    return inventoryCapabilityExternalUpAndSides.cast();
+            }
+        }
+
+        return super.getCapability(cap, side);
     }
 
     @Override
@@ -126,9 +182,48 @@ public class QuartzAssemblerTileEntity extends TileEntity implements ITickableTi
     }
 
     @Override
-    public void remove() {
-        super.remove();
+    public void read(CompoundNBT compound) {
+        super.read(compound);
 
+        this.inventory.deserializeNBT(compound.getCompound(INVENTORY_TAG));
     }
 
+    @Override
+    public CompoundNBT write(CompoundNBT compound) {
+        super.write(compound);
+
+        compound.put(INVENTORY_TAG, this.inventory.serializeNBT());
+
+        return compound;
+    }
+
+    /**
+     * Get an NBT compound to sync to the client with SPacketChunkData, used for initial loading of the
+     * chunk or when many blocks change at once.
+     * This compound comes back to you client-side in {@link #handleUpdateTag}
+     * The default implementation ({@link TileEntity#handleUpdateTag}) calls {@link #writeInternal)}
+     * which doesn't save any of our extra data so we override it to call {@link #write} instead
+     */
+    @Override
+    public CompoundNBT getUpdateTag() {
+        return this.write(new CompoundNBT());
+    }
+
+    @Override
+    public void remove() {
+        super.remove();
+        inventoryCapabilityExternal.invalidate();
+    }
+
+    @Override
+    public ITextComponent getDisplayName() {
+        return new TranslationTextComponent(ModBlocks.QUARTZ_ASSEMBLER.getTranslationKey());
+    }
+
+    @Nullable
+    @Override
+    public Container createMenu(int p_createMenu_1_, PlayerInventory p_createMenu_2_, PlayerEntity p_createMenu_3_) {
+        // TODO: Create menu..
+        return null;
+    }
 }
